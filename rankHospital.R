1fca7d5bf9cd3defd760b68a43ced83efc744a28
Kaley Regner <66963723+kalesreg@users.noreply.github.com>
2020-07-17 13:07:19 -0400

    rankHospital.R complete

--- rankHospital.R
+++ rankHospital.R
@@ -7,20 +7,54 @@
 rankhospital <- function(state, outcome, num = "best") {
         ## Read outcome data
         data <- read.csv("outcome-of-care-measures.csv")
+        ## Check that state and outcome are valid
         if (tolower(outcome) == "heart attack") {
                 column <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
-        }
-        if (tolower(outcome) == "heart failure") {
+        } else if (tolower(outcome) == "heart failure") {
                 column <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
-        }
-        if (tolower(outcome) == "pneumonia") {
+        } else if (tolower(outcome) == "pneumonia") {
                 column <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+        } else {
+                stop("invalid outcome")
         }
-        ## Check that state and outcome are valid
-        ## Return hospital name in that state with the given rank 
+        rel_hospitals <- data.frame()
+        data_added <- FALSE
+        for (i in 1:nrow(data)) {
+                state_data <- data[i, "State"]
+                outcome_data <- data[i, column]
+                if (state_data == state) {
+                        if (outcome_data != "Not Available") {
+                                hospital <- data[i, "Hospital.Name"]
+                                rel_col <- c(hospital, outcome_data)
+                                rel_hospitals <- rbind(rel_hospitals, rel_col)
+                                data_added <- TRUE
+                        } else if (i != nrow(data)) {
+                                next
+                        } else if (data_added == TRUE) {
+                                break
+                        } else {
+                                stop("invalid outcome")
+                        }
+                } else if (i != nrow(data)) {
+                        next
+                } else if (data_added == TRUE) {
+                        break
+                } else {
+                        stop("invalid state")       
+                }
+        }
+        ## Return hospital name in that state with the given rank
+        colnames(rel_hospitals) <- c("Hospital.Name", outcome)
+        ranked <- rel_hospitals[order(as.numeric(rel_hospitals[, outcome]), 
+                                rel_hospitals[, "Hospital.Name"]), ]
+        last <- nrow(ranked)
+        if (num == "best" || num == 1) {
+                print(ranked[1, "Hospital.Name"])
+        } else if (num == "worst") {
+                print(ranked[last, "Hospital.Name"])
+        } else if (num > last) {
+                print(NA)
+        } else {
+                print(ranked[num, "Hospital.Name"])
+        }
 }
-
-## Note: The num argument can take values “best”, “worst”, or an integer 
-## indicating the ranking (smaller numbers are better). If the number given by 
-## num is larger than the number of hospitals in that state, then the function 
-## should return NA.
